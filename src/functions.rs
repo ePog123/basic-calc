bc6073931638a6b80002bb1ab656f8dece56140f
Arnav Gyanani <gyananiarnav@gmail.com>
2022-06-11 17:17:06 -0700

    add files

---
+++ src/functions.rs
@@ -0,0 +1,12 @@
+pub fn add(n1: f64, n2: f64) -> f64 {
+    n1 + n2
+}
+pub fn div(n1: f64, n2: f64) -> f64 {
+    n1 / n2
+}
+pub fn sub(n1: f64, n2: f64) -> f64 {
+    n1 - n2
+}
+pub fn mlt(n1: f64, n2: f64) -> f64 {
+    n1 * n2
+}
