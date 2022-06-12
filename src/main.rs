8ce4a6061ae331cb106206eeee5941f96f5a5652
Arnav Gyanani <gyananiarnav@gmail.com>
2022-06-11 17:41:26 -0700

    Fix

--- src/main.rs
+++ src/main.rs
@@ -6,44 +6,43 @@ fn main() {
     let mut num2 = String::new();
     let mut function = String::new();
 
-    loop {
-        println!("Enter 1st number");
-        io::stdin()
-            .read_line(&mut num1)
-            .expect("Failed to read line");
-        let num1: f64 = match num1.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("Enter funtion");
-        io::stdin()
-            .read_line(&mut function)
-            .expect("Failed to read line");
-        println!("Enter 2st number");
-        io::stdin()
-            .read_line(&mut num2)
-            .expect("Failed to read line");
-        let num2: f64 = match num2.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+    println!("Enter 1st number");
+    io::stdin()
+        .read_line(&mut num1)
+        .expect("Failed to read line");
+    let num1: f64 = match num1.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Enter a number"),
+    };
+    println!("Enter function");
+    io::stdin()
+        .read_line(&mut function)
+        .expect("Failed to read line");
 
-        loop {
-            if function.to_string() == "+".to_string() {
-                println!("{}", functions::add(num1, num2));
-                break;
-            } else if function.to_string() == "*".to_string() {
-                println!("{}", functions::mlt(num1, num2));
-                break;
-            } else if function.to_string() == "/".to_string() {
-                println!("{}", functions::div(num1, num2));
-                break;
-            } else if function.to_string() == "-".to_string() {
-                println!("{}", functions::sub(num1, num2));
-                break;
-            } else {
-                panic!("Enter valid function")
-            }
+    println!("Enter 2nd number");
+    io::stdin()
+        .read_line(&mut num2)
+        .expect("Failed to read line");
+    let num2: f64 = match num2.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Enter a number"),
+    };
+
+    loop {
+        if function.trim() == "+".to_string() {
+            println!("{}", functions::add(num1, num2));
+            break;
+        } else if function.trim() == "*".to_string() {
+            println!("{}", functions::mlt(num1, num2));
+            break;
+        } else if function.trim() == "/".to_string() {
+            println!("{}", functions::div(num1, num2));
+            break;
+        } else if function.trim() == "-".to_string() {
+            println!("{}", functions::sub(num1, num2));
+            break;
+        } else {
+            panic!("Enter valid function")
         }
     }
 }
